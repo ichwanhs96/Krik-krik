980136c9ea54fe600dbfdd58d868f1073ff0cfdc
ichwanhs96 <ichwanharyosembodo96@gmail.com>
2015-03-15 18:42:23 +0700

    tugas 6 kelar

--- tugas6.cpp
+++ tugas6.cpp
@@ -37,12 +37,10 @@ int main() {
 
     /*** MAIN ***/
     /* Declarations */
-    bool modeTugas = false;
-    bool isZero = false;
     vector<Point> figure_point, modeTugas_point;
-    Color c;
+    Color c(255,255,255);
     char input_key;
-    Vector3i eyeView = {320, 240, 100};
+    Vector3i eyeView = {320, 0, 100};
     Vector3i P1[8];
     Vector3i P2[8];
     Vector3i P3[8];
@@ -85,9 +83,6 @@ int main() {
 	P4[7] = {540, 380, 500};
 
     /* Initialization */
-    c.R = 255;
-    c.G = 255;
-    c.B = 255;
 
     figure_point = figure::make3DCube(P1, eyeView);
 	grafika.draw(figure_point, c, "dynamic");
@@ -107,6 +102,7 @@ int main() {
 	modeTugas_point = grafika.getDisplayPoint("dynamic");
 
     while(input_key!='q'){
+		
 		figure_point = figure::make3DCube(P1, eyeView);
 		grafika.draw(figure_point, c, "dynamic");
 
@@ -125,108 +121,15 @@ int main() {
 		figure_point = figure::makeObject("frame.txt");
 		grafika.draw(figure_point, c, "static");
 		
-		//if(!modeTugas){
-			figure_point = figure::make3DCube(P1, eyeView);
-			grafika.draw(figure_point, c, "dynamic");
-			
-			figure_point = figure::make3DCube(P2, eyeView);
-			grafika.draw(figure_point, c, "dynamic");
-			
-			figure_point = figure::make3DCube(P3, eyeView);
-			grafika.draw(figure_point, c, "dynamic");
-			
-			figure_point = figure::make3DCube(P4, eyeView);
-			grafika.draw(figure_point, c, "dynamic");
-		//}
-		/*
-		if(modeTugas){
-			grafika.draw(modeTugas_point, c, "dynamic");
-		}
-		*/ 
-		
 		input_key = keyboard::getInput();
-		if(input_key == 32 && modeTugas)
-			modeTugas = false;
-		else if(input_key == 32 && !modeTugas){
-			modeTugas = true;
-			//modeTugas_point = grafika.getDisplayPoint("dynamic");
-		}
-		
-		if(!modeTugas){
-			if(input_key == 67)
-					eyeView.x += 10;
-			if(input_key == 68)
-				if(eyeView.x > 0)
-					eyeView.x -=10;
-			if(input_key == 65)
-					eyeView.y += 10;
-			if(input_key == 66)
-				if(eyeView.y > 0)
-					eyeView.y -= 10;
-		}
-		/*
-		else{
-			int iterator = modeTugas_point.size();
-			for(int i = 0; i < iterator; i++){
-				if(modeTugas_point[i].x >= 750 || modeTugas_point[i].y >= 450 || modeTugas_point[i].x <= 0 || modeTugas_point[i].y <= 0)
-					isZero = true;
-			}
-			
-			if(input_key == 67)
-				for(int i = 0; i < iterator; i++){
-					if(!isZero){
-						modeTugas_point[i].x -= 10;
-						modeTugas_point[i].y += 10;
-					}
-				}
-			if(input_key == 68)
-				for(int i = 0; i < iterator; i++){
-					if(!isZero){
-						modeTugas_point[i].x += 10;
-						modeTugas_point[i].y -= 10;
-					}
-				}
-		}
-		*/ 
+	
+		if(input_key == 67)
+			if(eyeView.x <= 650)
+				eyeView.x += 10;
+		if(input_key == 68)
+			if(eyeView.x > 0)
+				eyeView.x -=10;
 		 
-		 
-		else{
-			if(input_key == 67){
-				for(int i = 0; i<8; i++){
-					P1[i].x += 10;
-					P1[i].y += 10;
-					P2[i].x += 10;
-					P2[i].y += 10;
-					P3[i].x += 10;
-					P3[i].y += 10;
-					P4[i].x += 10;
-					P4[i].y += 10;
-					P4[i].y += 10;	
-				}
-			}
-			if(input_key == 68){
-				for(int i = 0; i<8; i++){
-					if(P1[i].x > 0 && P1[i].y > 0 && P2[i].x > 0 && P2[i].y > 0 && P3[i].x > 0 && P3[i].y > 0
-						&& P4[i].x > 0 && P4[i].y > 0){
-						isZero = false;
-					}
-					else isZero = true;
-				}
-				if(!isZero)
-					for(int i = 0; i<8; i++){
-						P1[i].x -= 10;
-						P1[i].y -= 10;
-						P2[i].x -= 10;
-						P2[i].y -= 10;
-						P3[i].x -= 10;
-						P3[i].y -= 10;
-						P4[i].x -= 10;
-						P4[i].y -= 10;
-					}
-			}
-		}
-
-		isZero = false;
 		grafika.clearScreen();
 		//printf("input key = %c kode = %d\n", input_key, input_key);
 		// atas = 65 bawah = 66 kanan = 67 kiri = 68 spasi = 32
